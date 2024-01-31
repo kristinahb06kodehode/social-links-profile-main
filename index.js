e24347556985a0b3adbe9c673501c428b6d93989
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2024-01-31 11:23:27 +0100

    startet js for eventlistener

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const listItem = document.querySelectorAll("list-item");
+
+  listItem.addEventListener("click", function (event) {});
+});
